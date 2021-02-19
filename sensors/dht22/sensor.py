845dd4d4b35ad1016152e56449fc6507ceb42426
Jason Young <jason@ytechie.com>
2021-02-19 20:41:18 +0000

    sensor script uses stdout

--- sensors/dht22/sensor.py
+++ sensors/dht22/sensor.py
@@ -1,12 +1,17 @@
 import Adafruit_DHT
+import json
+import sys
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Failed to retrieve data from humidity sensor")
+if humidity is not None and temperature is not None:
+    data = {}
+    data['TempC'] = temperature
+    data['Humidity'] = humidity
+    
+    sys.stdout.write(json.dumps(data))
+else:
+    sys.stderr.write("Failed to retrieve data from humidity sensor")
