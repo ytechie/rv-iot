8dfee7a391416919a566c8336deb2c296db34aef
Jason Young <jason@ytechie.com>
2021-02-21 19:51:47 +0000

    Working on vedirect sensor

--- sensors/ve_direct/sensor.py
+++ sensors/ve_direct/sensor.py
@@ -1,11 +1,12 @@
 import Vedirect
-import json
+import sys
 
 #totally untested code
 
 def on_victron_data_callback(data):
-    print(data)
-    sys.stdout.write(json.dumps(data))
+    #print(data)
+    #sys.stdout.write(data)
+    foo = 2
 
 ve = Vedirect.Vedirect()
 ve.read_data_callback(on_victron_data_callback)
