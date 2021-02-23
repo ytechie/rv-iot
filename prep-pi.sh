7e9751fba44ac15163ae844bd41a6aa0ac9778d9
Jason Young <jason@ytechie.com>
2021-02-23 17:11:17 +0000

    Added local influx / grafana commands

--- prep-pi.sh
+++ prep-pi.sh
@@ -1,3 +1,6 @@
+# This script is meant to be run on a Pi running Ubuntu ARM 64-bit
+# Set up your SD card using the Pi Imager: https://www.raspberrypi.org/software/
+
 sudo apt update
 sudo apt upgrade -y
 
@@ -19,4 +22,16 @@ sudo usermod -aG docker ubuntu
 #Allow docker to start at boot
 sudo systemctl enable docker.service
 
-docker run -d -p 1880:1880 -v node_red_data:/data --privileged --restart unless-stopped nr
+docker run -d -p 1880:1880 -v node_red_data:/data --privileged --restart unless-stopped ytechie/rv-pi-nodered
+
+#if you want to run influx and grafana locally
+docker run -i -d -p 8086:8086 \
+      -v /var/influxdb-storage:/var/lib/influxdb \
+      --restart unless-stopped \
+      influxdb
+
+docker run -d -p 3000:3000 -v grafana-storage:/var/lib/grafana \
+    --restart unless-stopped --user 472 grafana/grafana
+
+sleep 5 #give influx a few seconds to start up
+curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE rvdata"
